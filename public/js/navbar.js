1097aa3a00c5e2988da49bceb9fb0a0db32a3c1c
Harsh Prabhu <prabhuharsh15@gmail.com>
2019-01-07 17:29:41 +0530

    Add existing files

---
+++ public/js/navbar.js
@@ -0,0 +1,19 @@
+var home = document.getElementById("home");
+// var messages = document.getElementById("messages");
+// var friends = document.getElementById("friends");
+home.addEventListener("click",function(){
+	this.classList.add("active");
+	// messages.classList.remove("active");
+	// friends.classList.remove("active");
+});
+// messages.addEventListener("click",function(){
+// 	this.classList.add("active");
+// 	home.classList.remove("active");
+// 	friends.classList.remove("active");
+// });
+// friends.addEventListener("click",function(){
+// 	this.classList.add("active");
+// 	messages.classList.remove("active");
+// 	home.classList.remove("active");
+// });
+
